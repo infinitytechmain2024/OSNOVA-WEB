13027204220f9f2374eb90e66e05e5b97be12a7e
infinitytechmain2024 <infinitytechmain2024@gmail.com>
2026-08-15 21:02:15 +0300

    test 6

---
+++ .output/public/assets/kursy-BTrpcSeR.js
@@ -0,0 +1,1 @@
+import{ut as e}from"./blocks-B5BfPMKL.js";import{t}from"./education-courses-page-Dy2jOEDS.js";var n=e();function r(){return(0,n.jsx)(t,{title:`Курси`})}export{r as component};
--- .output/server/_ssr/ssr.mjs
+++ .output/server/_ssr/ssr.mjs
@@ -93,7 +93,7 @@ function renderErrorPage() {
 }
 var serverEntryPromise;
 async function getServerEntry() {
-	if (!serverEntryPromise) serverEntryPromise = import("./server-TVQL_KHb.mjs").then((m) => m.default ?? m);
+	if (!serverEntryPromise) serverEntryPromise = import("./server-Dz48L-F-.mjs").then((m) => m.default ?? m);
 	return serverEntryPromise;
 }
 async function normalizeCatastrophicSsrResponse(response) {
